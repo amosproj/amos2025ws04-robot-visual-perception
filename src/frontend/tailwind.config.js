c38db9cdfd3e9ab64bb592f4240ec42a38f4924a
sarib <saribstudent@gmail.com>
2025-11-20 22:38:36 +0100

    refactor: migrate frontend styling to Tailwind CSS
    Signed-off-by: sarib <saribstudent@gmail.com>

---
+++ src/frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2025 robot-visual-perception
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
